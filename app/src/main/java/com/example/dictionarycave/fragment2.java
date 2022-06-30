4f57207e938a78f99a4a9859fb5622af2a410242
AkshaySingh <akshaysingh8700@gmail.com>
2022-06-30 21:02:23 +0530

    v2 tablayout

--- app/src/main/java/com/example/dictionarycave/fragment2.java
+++ app/src/main/java/com/example/dictionarycave/fragment2.java
@@ -18,4 +18,11 @@ public class fragment2 extends Fragment {
     public fragment2() {
         // Required empty public constructor
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+    }
 }
--- app/src/main/java/com/example/dictionarycave/fragment3.java
+++ app/src/main/java/com/example/dictionarycave/fragment3.java
@@ -8,14 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment3#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class fragment3 extends Fragment {
 
     public fragment3() {
         // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment3, container, false);
     }
 }
